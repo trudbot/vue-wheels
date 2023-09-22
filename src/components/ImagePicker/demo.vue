861660a5fabc7831064cbbae13b222b614c6ab81
trudbot <SYP2185759039@163.com>
2023-09-22 20:09:08 +0800

    feat: 点击上传的头像组件

---
+++ src/components/ImagePicker/demo.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import ImagePicker from './index.vue'
+import {ref} from "vue";
+const path = ref('未选择');
+const url = ref('https://trudbot-md-img.oss-cn-shanghai.aliyuncs.com/202309221744484.jpg');
+const uploadState = ref(false);
+
+function uploadImage(image: string) {
+  path.value = image;
+  uploadState.value = true;
+  setTimeout(() => {
+    url.value = "https://trudbot-md-img.oss-cn-shanghai.aliyuncs.com/202309221957600.png";
+    uploadState.value = false;
+  }, 3000);
+}
+</script>
+
+<template>
+  <div class="img-picker-demo-container">
+    <div class="img">
+      <ImagePicker :url="url" @upload="uploadImage" :upload-state="uploadState"></ImagePicker>
+    </div>
+    <div class="selected-info">当前选择的图片为: {{path}}</div>
+  </div>
+</template>
+
+<style scoped lang="less">
+.img-picker-demo-container {
+
+  display: flex;
+  //justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+
+  .img {
+    margin-top: 20%;
+    width: 200px;
+    height: 200px;
+  }
+
+  .selected-info {
+    margin-top: 50px;
+    font-size: 30px;
+  }
+}
+</style>
---
+++ src/components/ImagePicker/index.vue
@@ -0,0 +1,78 @@
+<script setup lang="ts">
+
+const emit = defineEmits<{
+  (e: 'upload', path: string): void;
+}>();
+
+const props = defineProps<{
+  url: string;
+  uploadState: boolean;
+}>();
+
+function select(e: Event) {
+  emit('upload', (e.target as HTMLInputElement).value);
+}
+
+</script>
+
+<template>
+  <div class="fill-up img-picker-container" :class="{uploading: uploadState}">
+    <img :src="url" alt="avatar">
+    <input type="file" class="fill-up" @change="select" accept="image/*">
+  </div>
+</template>
+
+<style scoped lang="less">
+.fill-up {
+  width: 100%;
+  height: 100%;
+}
+
+.img-picker-container {
+  position: relative;
+  overflow: hidden;
+
+  &.uploading:after {
+    content: 'uploading';
+    background: hsla(0, 0%, 0%, 0.2);
+  }
+
+  &:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    box-shadow: inset 0 0 5px hsla(264, 46%, 23%, 0.5);
+    color: white;
+    font-size: 2em;
+    font-family: Monospaced, serif;
+  }
+
+  &:hover:after {
+    content: 'edit';
+    background: hsla(0, 0%, 0%, 0.2);
+  }
+
+  img {
+    border-radius: 50%;
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  input {
+    z-index: 1;
+    cursor: pointer;
+    opacity: 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+}
+</style>
