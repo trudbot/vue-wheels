302d4ce1821187ebed1d6678f0146e6b4efefd40
trudbot <SYP2185759039@163.com>
2023-09-30 22:05:40 +0800

    build: 自动化部署

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+// test
 export default defineConfig({
   base: '/vue-wheels/',
   plugins: [vue()],
